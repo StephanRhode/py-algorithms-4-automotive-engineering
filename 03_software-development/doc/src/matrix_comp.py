0b6d320292ab2eb8041c5482fa68a2d2dec1fcde
StephanRhode <st.rhode@posteo.de>
2020-02-21 10:16:05 +0100

    Completes Sphinx notebook
    Signed-off-by: StephanRhode <st.rhode@posteo.de>

---
+++ 03_software-development/doc/src/matrix_comp.py
@@ -0,0 +1,19 @@
+"""
+.. module:: matrix_comp
+
+The :py:mod:`matrix_comp` module provides ...
+"""
+
+def matrix_multiply(a, b):
+    """ The :py:func:`matrix_algebra.matrix_multiply` function computes
+    the product of two matrices.
+
+    Args:
+        a (numpy.ndarray): first matrix
+        b (numpy.ndarray): second matrix
+
+    Returns:
+        (numpy.ndarray): product of a and b
+
+    """
+    return a @ b
