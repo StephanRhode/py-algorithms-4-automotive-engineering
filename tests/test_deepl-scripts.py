e777cf8924174a180e86e1afebd7841ca875bac6
StephanRhode <st.rhode@posteo.de>
2020-03-05 18:35:57 +0100

    Bugfix
    Signed-off-by: StephanRhode <st.rhode@posteo.de>

--- tests/test_deepl-scripts.py
+++ tests/test_deepl-scripts.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 def test_regression():
-    subprocess.run("pytest 04_mini-projects/09_files/regression.py", shell=True)
+    subprocess.run("pytest 04_mini-projects/deepl_files/regression.py", shell=True)
 
 
 def test_classification():
-    subprocess.run("pytest 04_mini-projects/09_files/classification.py", shell=True)
+    subprocess.run("pytest 04_mini-projects/deepl_files/classification.py", shell=True)
