0b313c31c79ab27cabfb02a7e3ecdbe4e0e09a03
Stephan Rhode <st.rhode@posteo.de>
2020-04-06 20:07:58 +0200

    Adds scrolling to slides
    Signed-off-by: Stephan Rhode <st.rhode@posteo.de>

--- make_slides.py
+++ make_slides.py
@@ -4,5 +4,5 @@ import subprocess
 
 for notebook in glob.glob("**/*.ipynb"):
     print("make slides for: ", notebook)
-    command = "jupyter nbconvert " + notebook + " --to slides"
+    command = "jupyter nbconvert " + notebook + " --to slides --SlidesExporter.reveal_scroll=True"
     subprocess.run(command, shell=True)
